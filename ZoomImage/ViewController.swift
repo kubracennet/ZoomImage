82f253360404fc4806daaebc2de8bdc307b0c835
kubracennet <kubracennetyavasoglu@gmail.com>
2023-03-16 00:21:47 +0300

    Make a zoom in , zoom out . UIImageView

--- ZoomImage/ViewController.swift
+++ ZoomImage/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpScrollView()
+     }
+//Kaydırma görünümünü ayarlamak için func oluştururum
+    
+    func setUpScrollView() {
+//kaydırma görünümünü temsilciye uygun hale getiririm.
+        scrollView.delegate = self
     }
 
-
+}
+extension ViewController: UIScrollViewDelegate {
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
 }
 
